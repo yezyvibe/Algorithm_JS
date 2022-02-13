f2706fec0861e3290609fdc8a987461d467d49ea
yezyvibe <yeji0501@gmail.com>
2022-02-13 14:38:16 +0900

    p_가장큰수.js

---
+++ p_가장큰수.js
@@ -0,0 +1,12 @@
+function solution(numbers) {
+  numbers.reduce((a, b) => a + b) === 0
+    ? (answer = 0)
+    : (answer = numbers
+        .map((number) => String(number))
+        .sort((a, b) => b + a - (a + b))
+        .join(""));
+  return answer;
+}
+
+numbers = [0, 0, 0];
+console.log(solution(numbers));
